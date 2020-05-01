025b7524a605e4e48ca0269129169d6d314c294e
David Yusta <david.yusta@guidesmiths.com>
2020-05-01 21:03:06 +0200

    refactor store component structure

--- components/store/index.js
+++ components/store/index.js
@@ -1,12 +1,22 @@
 const System = require('systemic');
 const initMetadataStore = require('./metadataStore');
 const initMysqlStore = require('./mysqlStore');
-const initSakilaStore = require('./sakilaStore');
+const initObjectiveStore = require('./objectiveStore');
+const initRegisterStore = require('./registerStore');
 
 module.exports = new System({ name: 'store' })
 	.add('store.metadata', initMetadataStore())
 	.dependsOn('config', 'logger', 'mysql')
 	.add('store.mysql', initMysqlStore())
 	.dependsOn('config', 'logger', 'mysql')
-	.add('store.sakila', initSakilaStore())
-	.dependsOn('config', 'logger', 'mysql');
+	.add('store.objective', initObjectiveStore())
+	.dependsOn('config', 'logger', 'mysql')
+	.add('store.register', initRegisterStore())
+	.dependsOn('config', 'logger', 'mysql')
+	.add('store')
+	.dependsOn(
+		{ component: 'store.metadata', destination: 'metadata' },
+		{ component: 'store.mysql', destination: 'mysql' },
+		{ component: 'store.objective', destination: 'objective' },
+		{ component: 'store.register', destination: 'register' },
+	);
