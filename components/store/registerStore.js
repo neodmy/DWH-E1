9f97b73d52dd63140ea35111a3c1800f789c4ec7
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-05-02 18:54:16 +0200

    refactor and complete registerStore component (#7)

--- components/store/registerStore.js
+++ components/store/registerStore.js
@@ -1,25 +1,37 @@
 module.exports = () => {
 	const start = async ({ config, logger, mysql }) => {
-		logger.info('Starting registerStore component');
 		const { registerSchema } = mysql;
 		const { dbName } = config;
 
 		const initStore = async () => {
-			await registerSchema.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`);
-			await registerSchema.query(`USE ${dbName}`);
+			logger.info('register store | Initiating register schema');
+			const createQuery = `CREATE DATABASE IF NOT EXISTS ${dbName}`;
+			const useQuery = `USE ${dbName}`;
+			logger.info('register store | initiating');
+			await registerSchema.query(createQuery);
+			await registerSchema.query(useQuery);
 		};
 
-		const executeDrop = dropQueries => Promise.all(dropQueries.map(dropQuery => registerSchema.query(dropQuery)));
+		const executeDrop = dropQueries => {
+			logger.info('register store | dropping register schema tables');
+			return Promise.all(dropQueries.map(dropQuery => registerSchema.query(dropQuery)));
+		};
 
-		const executeCreate = createQueries => Promise.all(createQueries.map(async createQuery => {
-			await registerSchema.query(createQuery.before);
-			await registerSchema.query(createQuery.after);
-		}));
+		const executeCreate = createQueries => {
+			logger.info('register store | creating register schema tables');
+			return Promise.all(createQueries.map(async createQuery => {
+				await registerSchema.query(createQuery.before);
+				await registerSchema.query(createQuery.after);
+			}));
+		};
+
+		const executeInsertBefore = (query, data) => registerSchema.query(query, [data]);
 
 		return {
 			initStore,
 			executeDrop,
 			executeCreate,
+			executeInsertBefore,
 		};
 	};
 
