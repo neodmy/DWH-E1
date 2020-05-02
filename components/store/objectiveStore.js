bcb73fca9f1844b5511868ffa879464286977068
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-05-02 21:50:32 +0200

    Objective store (#9)
    * modify test name
    
    * add execute query
    
    * add tests for objectiveStore
    
    * add objective db name as variable
    
    * skip test

--- components/store/objectiveStore.js
+++ components/store/objectiveStore.js
@@ -1,15 +1,27 @@
+/* eslint-disable consistent-return */
 module.exports = () => {
 	const start = async ({ config, logger, mysql }) => {
 		const { objectiveSchema } = mysql;
 		const { dbName } = config;
+		logger.info(`Starting ${dbName}Store component`);
 
 		const initStore = async () => {
+			logger.info(`${dbName} store | Initiating ${dbName} schema`);
 			await objectiveSchema.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`);
 			await objectiveSchema.query(`USE ${dbName}`);
 		};
 
+		const executeQuery = async query => {
+			if (!query) return;
+			logger.info(`${dbName} store | executing query: 
+				${query}`);
+			const result = await objectiveSchema.query(query);
+			return result;
+		};
+
 		return {
 			initStore,
+			executeQuery,
 		};
 	};
 
---
+++ test/objective.test.js
@@ -0,0 +1,26 @@
+const system = require('../system');
+
+describe('Objective store', () => {
+	const sys = system();
+	let store;
+
+	before(async () => {
+		({ store, config } = await sys.start());
+		await store.objective.initStore();
+	});
+
+	after(() => sys.stop());
+
+	describe('Execute query', () => {
+		it.skip('should execute a query without errors', async () => {
+			const query = 'CREATE TABLE IF NOT EXISTS pet (name VARCHAR(20), owner VARCHAR(20))';
+			const query2 = 'INSERT INTO pet (name, owner) VALUES (\'dayus\',\'rocky\')';
+			const query3 = 'SELECT * FROM pet';
+			const query4 = 'DELETE FROM pet';
+			await store.objective.executeQuery(query);
+			await store.objective.executeQuery(query2);
+			await store.objective.executeQuery(query3);
+			await store.objective.executeQuery(query4);
+		});
+	});
+});
