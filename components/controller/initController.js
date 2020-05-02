867bb0c0bcf58ba74e298dcc41ff84f56a4b72b9
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-05-02 23:43:21 +0200

    refactor controller to save metadata only after successful query (#11)

--- components/controller/initController.js
+++ components/controller/initController.js
@@ -10,31 +10,27 @@ module.exports = () => {
 		const dropQueries = await metadata.dropAllTablesQueries();
 		const createQueries = await metadata.createAllTablesQueries();
 
-		const snapshotBefore = () => {
-			logger.info('controller | taking snapshot before query');
-			return Promise.all(Object.keys(metadata.informationSchemaTables)
-				.map(async tableName => {
-					const table = metadata.informationSchemaTables[tableName];
-					logger.info(`metadata store | select all data from information_schema.${tableName}`);
-					const data = await table.select();
-					logger.info(`register store | inserting data (${data.length} rows) into register.${tableName}_BEFORE`);
-					const insertQuery = await table.insert();
-					await register.executeInsertBefore(insertQuery.before, data);
-				}));
-		};
+		const selectMetadata = () => Promise.all(Object.keys(metadata.informationSchemaTables)
+			.map(async tableName => {
+				const table = metadata.informationSchemaTables[tableName];
+				return table.select();
+			}));
 
-		const snapshotAfter = () => {
-			logger.info('controller | taking snapshot after query');
-			return Promise.all(Object.keys(metadata.informationSchemaTables)
-				.map(async tableName => {
-					const table = metadata.informationSchemaTables[tableName];
-					logger.info(`metadata store | select all data from information_schema.${tableName}`);
-					const data = await table.select();
-					logger.info(`register store | inserting data (${data.length} rows) into register.${tableName}_AFTER`);
-					const insertQuery = await table.insert();
-					await register.executeInsertBefore(insertQuery.after, data);
-				}));
-		};
+		const insertMetadataBefore = data => Promise.all(Object.keys(metadata.informationSchemaTables)
+			.map(async (tableName, index) => {
+				const table = metadata.informationSchemaTables[tableName];
+				logger.info(`register store | inserting data (${data[index].length} rows) into register.${tableName}_BEFORE`);
+				const insertQuery = await table.insert();
+				await register.executeInsertBefore(insertQuery.before, data[index]);
+			}));
+
+		const insertMetadataAfter = data => Promise.all(Object.keys(metadata.informationSchemaTables)
+			.map(async (tableName, index) => {
+				const table = metadata.informationSchemaTables[tableName];
+				logger.info(`register store | inserting data (${data[index].length} rows) into register.${tableName}_AFTER`);
+				const insertQuery = await table.insert();
+				await register.executeInsertBefore(insertQuery.after, data[index]);
+			}));
 
 		const cleanRegisterTables = async () => {
 			logger.info('controller | cleaning register tables');
@@ -44,9 +40,12 @@ module.exports = () => {
 
 		const recordMetadata = async query => {
 			await cleanRegisterTables();
-			await snapshotBefore();
+			const metadataBefore = await selectMetadata();
 			const result = await objective.executeQuery(query);
-			await snapshotAfter();
+			const metadataAfter = await selectMetadata();
+			await insertMetadataBefore(metadataBefore);
+			await insertMetadataAfter(metadataAfter);
+			logger.info(`controller | metadata update for query: ${query}`);
 			return result;
 		};
 
